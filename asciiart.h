7f41d6e63266b8fd6b56bf3ae9b54056ff5236b0
Pauis <ciqete@gmail.com>
2015-06-27 07:58:43 +0000

    Modified asciiart.h

--- asciiart.h
+++ asciiart.h
@@ -1,4 +1,9 @@
 #ifndef __ASCIIART_H__
 #define __ASCIIART_H__
 
+extern void ArtMain(void);
+extern void ArtAttackA(void);
+extern void ArtAttackB(void);
+extern void ArtAttackTogether(void);
+
 #endif
